d0984c20da1980ee0ece2de206681ac6ec253d7c
danah2139 <danah2139@gmail.com>
2022-04-03 16:46:16 +0300

    add tests

--- src/task2/ecommerce.test.js
+++ src/task2/ecommerce.test.js
@@ -1,7 +1,18 @@
 import { fetchOrderById } from "../api";
+import { fetchAllOrders, bucketOrdersByUsers } from "./ecommerce";
 
-const ORDER_ID = "70ef599e5eca171b2bce84d1"
+const ORDER_ID = "70ef599e5eca171b2bce84d1";
 test("Ecommerce - fetchOrderById", async () => {
-    let orders = await fetchOrderById(ORDER_ID);
-    expect(orders).toBeTruthy();
+  let orders = await fetchOrderById(ORDER_ID);
+  expect(orders).toBeTruthy();
+});
+
+test("Ecommerce - fetchAllOrders", () => {
+  let orders = fetchAllOrders();
+  expect(orders).toBeTruthy();
+});
+
+test("Ecommerce - bucketOrdersByUsers", () => {
+  let orders = bucketOrdersByUsers();
+  expect(orders).toBeTruthy();
 });
