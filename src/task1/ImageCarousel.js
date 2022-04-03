9bb55c35f35c46aaaafa130d951ac2f4c11a767e
danah2139 <danah2139@gmail.com>
2022-04-03 15:57:58 +0300

    test1+test2

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,59 @@
 import React from "react";
 import { fetchImageUrls } from "../api/index";
+import { useState, useEffect } from "react";
+import "./styles.css";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+  const [images, setImages] = useState();
+  const [imageIndex, setImagesIndex] = useState(0);
+
+  useEffect(
+    (async () => {
+      setImages(await fetchImageUrls());
+    })(),
+    []
+  );
+  console.log(images);
+  const fetchImages = () => {
+    return images ? (
+      <img className="image" src={images[imageIndex]} alt="" />
+    ) : null;
+  };
+  const nextImage = () => {
+    if (!images.length) {
+      return;
+    }
+    setImagesIndex((currentIndex) => {
+      if (currentIndex === images.length - 1) {
+        return 0;
+      }
+      return currentIndex + 1;
+    });
+  };
+
+  const previousImage = () => {
+    if (!images.length) {
+      return;
+    }
+    setImagesIndex((currentIndex) => {
+      if (currentIndex === 0) {
+        return images.length - 1;
+      }
+      return currentIndex - 1;
+    });
+  };
+  return (
+    <div className="container">
+      <button onClick={previousImage}>
+        <ArrowBackIosIcon />
+      </button>
+      {fetchImages()}
+      <button onClick={nextImage}>
+        <ArrowForwardIosIcon />
+      </button>
+    </div>
+  );
 };
 export default ImageCarousel;
--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -4,39 +4,65 @@ import { allIds, fetchOrderById } from "../api";
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
 
 const fetchAllOrders = () => {
-    const ids = allIds;
-    // .....
-    //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+  const ids = allIds;
+  // .....
+  //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+  const fetchPromises = ids.map((id) => fetchOrderById(id));
+  return Promise.all(fetchPromises);
 };
 
-const bucketOrdersByUsers = () => {
-    let ordersByUsers = {};
-    //   2. TODO: using the function from section 1 you should now bucket the orders by user.
-    // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
-    return ordersByUsers;
+const bucketOrdersByUsers = async () => {
+  let ordersByUsers = {};
+  //   2. TODO: using the function from section 1 you should now bucket the orders by user.
+  // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+  const orders = await fetchAllOrders();
+  orders.forEach((order) => {
+    if (ordersByUsers[order.userId]) {
+      ordersByUsers[order.userId].push(order);
+    } else {
+      ordersByUsers[order.userId] = [order];
+    }
+  });
+  return ordersByUsers;
 };
 
 const getLast2WeeksOrders = () => {
-    //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+  //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+  const orders = await fetchAllOrders();
+  const last2WeeksOrders = [];
+  var twoWeeksMS = 1000 * 60 * 60 * 24 * 14;
+  const twoWeeksAgoTimestamp = new Date() - twoWeeksMS;
+  orders.forEach(order => {
+      if (order.timestamp > twoWeeksAgoTimestamp) {
+          last2WeeksOrders.push(order);
+      }
+    });
+    return last2WeeksOrders;
+
 };
 
 const bucketOrdersByDate = () => {
-    let ordersByDate = {};
-    //   4. TODO: using the function from section 3 bucket the orders by date.
-    // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
-    return ordersByDate;
+  let ordersByDate = {};
+  //   4. TODO: using the function from section 3 bucket the orders by date.
+  // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
+  const last2weeksOrders = await getLast2WeeksOrders();
+  last2weeksOrders.forEach(order => {
+      const orderDate = new Date(order.timestamp).getDate();
+      if (ordersByDate[orderDate]) {
+          ordersByDate[orderDate].push(order);
+      } else {
+          ordersByDate[orderDate] = [order];
+      }
+  });
+  return ordersByDate;
 };
 
-fetchAllOrders();
-// .then(console.log);
+fetchAllOrders().then(orders=>console.log(orders));
 
-bucketOrdersByUsers();
-// .then(console.log);
+bucketOrdersByUsers().then(ordersByUsers=>console.log(ordersByUsers));
 
-getLast2WeeksOrders();
-// .then(console.log);
+getLast2WeeksOrders().then(last2WeeksOrders=>console.log(last2WeeksOrders));
 
-bucketOrdersByDate();
-// .then(console.log);
+bucketOrdersByDate().then(orderrsByDate=>console.log(orderrsByDate));
 
 ////////////////////////////////////////
